c0c29fca3981cd878afb6b95116ac1165a916b25
bekonekooo <136001183+bekonekooo@users.noreply.github.com>
2023-12-05 00:09:35 +0300

    Create java.js

---
+++ görevler/java.js
@@ -0,0 +1,31 @@
+function sum(){
+    var a=document.getElementById("number1").value;
+    var b=document.getElementById("number2").value;
+    a=parseInt(a);
+    b=parseInt(b);
+    c=a+b;
+    alert(c);
+}
+function minus(){
+    var a=document.getElementById("number1").value;
+    var b=document.getElementById("number2").value;
+    a=parseInt(a);
+    b=parseInt(b);
+    c=a-b;
+    alert(c);
+}
+function divide(){
+    var a=document.getElementById("number1").value;
+    var b=document.getElementById("number2").value;
+    a=parseInt(a);
+    b=parseInt(b);
+    c=a/b;
+    alert(c);
+}
+function times(){
+    var a=document.getElementById("number1").value;
+    var b=document.getElementById("number2").value;
+    a=parseInt(a);
+    b=parseInt(b);
+    c=a*b;
+    alert(c);}
